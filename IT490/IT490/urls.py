cccd8bb7d945df6600969242124a383663d809a4
stealthflamez <jeff.a.ortiz@hotmail.com>
2020-10-19 18:00:53 -0400

    updates

--- IT490/IT490/urls.py
+++ IT490/IT490/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from HelloWorld.views import myView
+from .import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('HelloWorld/', myView),
+    path('login/', views.login),
+    path('', views.homepage)
+    
 ]
---
+++ IT490/IT490/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    #return HttpResponse('home')
+
+def login(request):
+    #return HttpResponse('login')
