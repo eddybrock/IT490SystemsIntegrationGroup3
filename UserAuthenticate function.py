cf3392910319cb998b7bd0f8d2ae9b18f53f812b
rmj100 <50150176+rmj100@users.noreply.github.com>
2020-11-01 21:31:33 -0500

    Update UserAuthenticate function.py

--- UserAuthenticate function.py
+++ UserAuthenticate function.py
@@ -3,59 +3,52 @@ import pika, os, sys
 import mysql.connector
 import json
 
+def callback (ch, method, propertues, body):
+    info = ("%r" % body)
+    info = info.strip("'")
+    info = json.loads(info)
+    if "email" in info:
+        add_users(info)
+    else:
+        authenticate(info)
+
+def add_users (instruction):
+    if "@gmail.com" in instruction["email"] or "@yahoo.com" in instruction["email"] or "@aol.com" in instruction["email"]:
+        query = ("INSERT INTO Users (username, email, password) VALUES (%s, %s, %s)")
+        mycursor.execute(query, (instruction["username"], instruction["email"], instruction["password"],))
+        mydb.commit()
+        channel2.basic_publish(exchange='', routing_key='confrim', body='user added')
+    else:
+        channel2.basic_publish(exchange='', routing_key='confrim', body='Invaild email')
+
+def authenticate (instruction):
+    query = ("SELECT * FROM Users WHERE username =%s AND password =%s")
+    mycursor.execute(query, (instruction["username"], instruction["password"],))
+    results = mycursor.fetchall()
+    if mycursor.rowcount == 1:
+        channel2.basic_publish(exchange='', routing_key='confrim', body='Correct username and password')
+    else:
+        channel2.basic_publish(exchange='', routing_key='confrim', body='Username and password are incorrect')
+
 def main():
-#connect to the mysql database 
-    mydb = mysql.connector.connect(user="user1", password="Password1234", host = "localhost", database="StockGame")
+    global mydb
+    global mycursor
+    mydb = mysql.connector.connect(user="user1", password="Password1234", host = "localhost", database = "StockGame")
     mycursor = mydb.cursor()
 
-#set up a connection to rabbitMQ
-#channel: is used to recieve messages from a chosen queue and listen to the queue called "hello"
-#channel2: is used to talk to a chosen queue and talks to the queue called "confrim"
-	connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host = 'localhost'))
+    global channel
+    global channel2
     channel = connection.channel()
     channel2 = connection.channel()
 
-#these lines tell rabbitMQ to create the queues and can be deleted if the queues already exist
-	channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='hello')
     channel2.queue_declare(queue='confrim')
-	
-	
-	
-	
-	
 
-#the function callback is called when a message is recieve from "the queue" 
-#the function reads the message (which should be the username and password in JSON) and then is sent to the database to authenticate
-#if the username and password match a record in the database mycursor.rowcount will hold the value 1 and send "True" to rabbitMQ 
-#if there is no match or somehow there more then one record chosen callback will send "False" to rabbitMQ
-     def callback (ch, method, properties, body):
-        info = ("%r" % body)
-        info = info.strip("'")
-        info = json.loads(info)
-        if "email" in info:
-            if "@gmail.com" in info["email"] or "@yahoo.com" in info["email"] or "@aol.com" in info["email"]:
-                query = ("INSERT INTO Users (username, email, password) VALUES (%s, %s, %s)")
-                mycursor.execute(query, (info["username"], info["email"], info["password"],))
-                mydb.commit()
-                channel2.basic_publish(exchange='', routing_key='confrim', body='user added')
-            else:
-                channel2.basic_publish(exchange='', routing_key='confrim', body='Invaild email')
-        else:
-            query = ("SELECT * FROM Users WHERE username =%s AND password = %s")
-            mycursor.execute(query, (info["username"], info["password"],))
-            results = mycursor.fetchall()
-            if mycursor.rowcount == 1:
-                channel2.basic_publish(exchange='', routing_key='confrim', body='Correct username and password')
-                connection.close()
-            else:
-                channel2.basic_publish(exchange='', routing_key='confrim', body='Username or password is incorrect')
-
-    
-#these two lines start the consume process on the queue "hello"    
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
-	channel.start_consuming()
+    channel.start_consuming()
 
-#keeps the script from closing out which allows for multiple messages to come thru
+
 if __name__ == '__main__':
     try:
         main()
